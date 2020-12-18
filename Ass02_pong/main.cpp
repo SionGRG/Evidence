6edf64e5334fd7fa4483d097ea7efe2c82b95806
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2020-12-18 02:05:55 +0000

    Update 1.1
    + Ass02_Pong InProgress +
    * Responded the feedback and updated the dev blog
    * Ass02_pong
      - less Hacky code
      - added comments to explain what is happening
      - Worked on bouncing the ball back on the player's bat
      - Implemented the feedback suggestions of having one bat class and creating a game class to simplify the main() function

--- Ass02_pong/main.cpp
+++ Ass02_pong/main.cpp
@@ -51,7 +51,15 @@ public:
 		window.draw(spr);
 	}
 	virtual void Update(sf::RenderWindow& window, float dT) = 0;
-	virtual void Init(sf::RenderWindow& window, sf::Texture& tex) = 0;
+	/*	Build a texture and set sprite position
+	float batTexPosX;		set the texture's rectangular x position value
+	float batTexPosY;		set the texture's rectangular y position value
+	float batTexWidth;		set the texture's rectangular width value
+	float batTexHeight;		set the texture's rectangular height value
+	float batPosX;			set the sprite's x position relative to the window size ie. a value that will be multiplied with the window size x value (must be between 0.f and 1.f)
+	float batPosY;			set the sprite's y position relative to the window size ie. a value that will be multiplied with the window size y value (must be between 0.f and 1.f)
+	*/
+	virtual void Init(sf::RenderWindow& window, sf::Texture& tex, int batTexPosX, int batTexPosY, int batTexWidth, int batTexHeight, float batPosX, float batPosY) = 0;
 };
 
 class Ball : public Obj2D
@@ -59,22 +67,26 @@ class Ball : public Obj2D
 private:
 public:
 	float xVel, yVel;
-	float SPEED = 100;
+	float SPEED = 350;
 	float xDir = 1;
 	float yDir = 1;
-	bool cDir = false;
 
 	int GetRandomNumber(int high, int low)
 	{
 		return (rand() % high) + low;
 	}
 
-	void Init(sf::RenderWindow& window, sf::Texture& tex) override
+	void Init(sf::RenderWindow& window, sf::Texture& tex, int batTexPosX, int batTexPosY, int batTexWidth, int batTexHeight, float batPosX, float batPosY) override
 	{
 		spr.setTexture(tex, true);
-		spr.setTextureRect(sf::IntRect{ 105, 5, 40, 40 });
-		spr.setOrigin(20, 20);
-		spr.setPosition(window.getSize().x * 0.5f, window.getSize().y * 0.5f);
+		//Hint: spr.setTextureRect(sf::IntRect{ 105, 5, 40, 40 });
+		spr.setTextureRect(sf::IntRect{ batTexPosX, batTexPosY, batTexWidth, batTexHeight });
+		//Hint: spr.setOrigin(20, 20);
+		float batOrgX = batTexWidth / 2;			// set origin().x to the center of the texture
+		float batOrgY = batTexHeight / 2;			// set origin().y to the center of the texture
+		spr.setOrigin(batOrgX, batOrgY);
+		//Hint: spr.setPosition(window.getSize().x * 0.5f, window.getSize().y * 0.5f);
+		spr.setPosition(window.getSize().x * batPosX, window.getSize().y * batPosY);
 
 		// Get random numbers for the ball movement
 		srand(time(NULL));
@@ -83,52 +95,24 @@ public:
 	void Reset(sf::RenderWindow& window)
 	{
 		spr.setPosition(window.getSize().x * 0.5f, window.getSize().y * 0.5f);
+		ChangeXDirection();
 	}
 
-	void Score(sf::RenderWindow& window, float dT)
+	void ChangeXDirection()
 	{
-
+		xDir *= -1;		// Change the direction of the ball in the x direction
 	}
-	void Record(sf::RenderWindow& window, float dT)
-	{	
-		// Record player score and reset the ball position
-		if (spr.getPosition().x < (window.getSize().x * 0) + spr.getOrigin().x)
-		{
-			Score(window, dT);
-			Reset(window);
-		}
-		if ((spr.getPosition().x + spr.getOrigin().x) > window.getSize().x)
-		{
-			Score(window, dT);
-			Reset(window);
-		}
-	}
-	/*
-	float ChangeXDirection(float cDirX)
-	{
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-			cDirX = cDirX * -1;
-			return cDirX;
-	}
-	*/
-	float ChangeXDirection(float cDirX)
-	{
-		cDirX *= -1;
-		return cDirX;
-		
-	}
-
 	float ChangeYDirection(sf::RenderWindow& window, float &cDirY)
 	{
 		// Check for hitting walls
 		if (spr.getPosition().y < (window.getSize().y * 0) + spr.getOrigin().y)
 		{
-			cDirY = cDirY * -1;
+			cDirY = cDirY * -1;				// bounce the ball off the wall
 			return cDirY;
 		}
 		if ((spr.getPosition().y + spr.getOrigin().y) > window.getSize().y)
 		{
-			cDirY = cDirY * -1;
+			cDirY = cDirY * -1;				// bounce the ball off the wall
 			return cDirY;
 		}
 	}
@@ -140,21 +124,18 @@ public:
 		yVel = GetRandomNumber(2, -2);		
 
 		ChangeYDirection(window, yDir);
-		if (cDir = true)
-			ChangeXDirection(xDir);
 
 		// Simulation
 		vel = Vec2{ xDir * xVel * SPEED , yDir * yVel * SPEED };
 		Vec2 pos{ spr.getPosition() };
 		pos += vel * dT;
-
+		
 		spr.setPosition(pos);
 	}
 
 	void Update(sf::RenderWindow& window, float dT) override
 	{
 		Movement(window, dT);
-		Record(window, dT);
 	}
 	void Render(sf::RenderWindow& window, float dT) override
 	{
@@ -164,76 +145,56 @@ public:
 	}
 };
 
-class Bat1 : public Obj2D
+class Bat : public Obj2D
 {
 public:
-	
-	Ball* pMyBall;
-	/*
-	void Init(sf::RenderWindow& window, sf::Texture& tex, Ball& ball)
-	{
-		pMyBall = &ball;
-		Init(window, tex);
-	}*/
+	// Controls
+	sf::Keyboard::Key moveUp;
+	sf::Keyboard::Key moveDown;
 
-	void Init(sf::RenderWindow& window, sf::Texture& tex)
+	void Init(sf::RenderWindow& window, sf::Texture& tex, int batTexPosX, int batTexPosY, int batTexWidth, int batTexHeight, float batPosX, float batPosY)
 	{
 		spr.setTexture(tex, true);
-		//spr.setTextureRect(sf::IntRect{ 105, 103, 143, 48 });
-		spr.setTextureRect(sf::IntRect{ 4, 5, 42, 140 });
-		spr.setOrigin(21, 70);
-		//spr.setPosition(window.getSize().x * 0.95f, window.getSize().y * 0.5f);
-		spr.setPosition(window.getSize().x * 0.025f, window.getSize().y * 0.5f);
+		//Hint: example: spr.setTextureRect(sf::IntRect{ 4, 5, 42, 140 });
+		spr.setTextureRect(sf::IntRect{ batTexPosX, batTexPosY, batTexWidth, batTexHeight });
+		//Hint: example: spr.setOrigin(21, 70);
+		float batOrgX = batTexWidth/2;					// set origin().x to the center of the texture
+		float batOrgY = batTexHeight/2;					// set origin().y to the center of the texture
+		spr.setOrigin(batOrgX, batOrgY);
+		//Hint: example: spr.setPosition(window.getSize().x * 0.05f, window.getSize().y * 0.5f);
+		spr.setPosition(window.getSize().x * batPosX, window.getSize().y * batPosY);
 	}
 
-	bool Bounce(Vec2 oBPos, Vec2 oBOrg)
+	void Movement(sf::RenderWindow& window, float dT)
 	{
-		// Check for hitting bat
-		/*if (oBPos.x < (pMyBall->spr.getPosition().x)
-			&& oBPos.y - oBOrg.y >(pMyBall->spr.getPosition().y)
-			&& oBPos.y + oBOrg.y < (pMyBall->spr.getPosition().y))*/
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		//{
-		//	pMyBall->cDir = true;
-		//	return pMyBall->cDir;
-		//}
-	}
-	void Update(sf::RenderWindow& window, float dT)
-	{
-
-		// Control
+		// Bat movement control
 		float SPEED = 350;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		if (sf::Keyboard::isKeyPressed(moveUp))
 		{
 			if ((spr.getPosition().y + 13) <= spr.getOrigin().y)
-				SPEED = 0;		// Stop the bat if it reaches to the top of the screen
+				SPEED = 0;					// Stop the bat if it reaches to the top of the screen
 			else
 				SPEED = SPEED;
 			vel = Vec2{ 0, -SPEED };
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		else if (sf::Keyboard::isKeyPressed(moveDown))
 		{
 			if ((spr.getPosition().y - 13) >= (window.getSize().y - spr.getOrigin().y))
-				SPEED = 0;		// Stop the bat if it reaches to the bottom of the screen
+				SPEED = 0;					// Stop the bat if it reaches to the bottom of the screen
 			else
 				SPEED = SPEED;
 			vel = Vec2{ 0, SPEED };
 		}
+	}
+	void Update(sf::RenderWindow& window, float dT)
+	{		
+		Movement(window, dT);
 		// Simulation
 		Vec2 pos{ spr.getPosition() };
 		pos += vel * dT;
 
 		spr.setPosition(pos);
 		
-		Vec2 bPos = pos;
-		Vec2 bOrg = spr.getOrigin();
-
-		// Bounce the ball back
-		//Bounce(bPos, bOrg);
-		/*if (pMyBall)
-		{
-			pMyBall->ChangeXDirection();
-		}*/
 	}
 	void Render(sf::RenderWindow& window, float dT) override
 	{
@@ -241,48 +202,94 @@ public:
 	}
 };
 
-class Bat2 : public Obj2D
+class Game
 {
+private:
+
 public:
+	Ball ball;
+	Bat bat1;
+	Bat bat2;
 
 	void Init(sf::RenderWindow& window, sf::Texture& tex)
 	{
-		spr.setTexture(tex, true);
-		spr.setTextureRect(sf::IntRect{ 54, 5, 42, 140 });
-		spr.setOrigin(21, 70);
-		spr.setPosition(window.getSize().x * 0.975f, window.getSize().y * 0.5f);
+
+		// Ball
+		ball.Init(window, tex, 105, 5, 40, 40, 0.5f, 0.5f);
+
+		// Players
+		bat1.Init(window, tex, 4, 5, 42, 140, 0.05f, 0.5f);
+		bat2.Init(window, tex, 54, 5, 42, 140, 0.95f, 0.5f);
 	}
+
+
+	void Score(sf::RenderWindow& window, float dT)
+	{
+
+	}
+	void Record(sf::RenderWindow& window, float dT)
+	{
+		// Record player score and reset the ball position goes the beyond the player's bat
+		if (ball.spr.getPosition().x < (window.getSize().x * 0) + ball.spr.getOrigin().x)
+		{
+			Score(window, dT);
+			ball.Reset(window);
+		}
+		if ((ball.spr.getPosition().x + ball.spr.getOrigin().x) > window.getSize().x)
+		{
+			Score(window, dT);
+			ball.Reset(window);
+		}
+	}
+
+	void controls()
+	{
+		// set the player controls
+		//player 1
+		bat1.moveUp = sf::Keyboard::W;
+		bat1.moveDown = sf::Keyboard::S;
+		//player 2
+		bat2.moveUp= sf::Keyboard::Up;
+		bat2.moveDown = sf::Keyboard::Down;
+	}
+
+	void Bounce(Bat bBat1, Bat bBat2)
+	{
+		// Check for hitting bat
+		if ((ball.spr.getPosition().x - ball.spr.getOrigin().x) < bBat1.spr.getPosition().x &&				// ball has gone too far right of the player's bat
+			(ball.spr.getPosition().y) < bBat1.spr.getPosition().y + bBat1.spr.getOrigin().y &&			
+			(ball.spr.getPosition().y) > bBat1.spr.getPosition().y - bBat1.spr.getOrigin().y)				// ball is in the height range of the player's bat
+		{
+			ball.ChangeXDirection();
+		}
+		else if ((ball.spr.getPosition().x + ball.spr.getOrigin().x) > bBat2.spr.getPosition().x &&			// ball has gone too far right of the player's bat
+			(ball.spr.getPosition().y) < bBat2.spr.getPosition().y + bBat2.spr.getOrigin().y &&			
+			(ball.spr.getPosition().y) > bBat2.spr.getPosition().y - bBat2.spr.getOrigin().y)				// ball is in the height range of the player's bat
+		{
+			ball.ChangeXDirection();
+		}
+	}
+
 	void Update(sf::RenderWindow& window, float dT)
 	{
-		// Control
-		float SPEED = 350;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		{
-			if ((spr.getPosition().y + 13) <= spr.getOrigin().y)
-				SPEED = 0;		// Stop the bat if it reaches to the top of the screen
-			else
-				SPEED = SPEED;
-			vel = Vec2{ 0, -SPEED };
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		{
-			if ((spr.getPosition().y - 13) >= (window.getSize().y - spr.getOrigin().y))
-				SPEED = 0;		// Stop the bat if it reaches to the bottom of the screen
-			else
-				SPEED = SPEED;
-			vel = Vec2{ 0, SPEED };
-		}
+		controls();
+		// Updates
+		ball.Update(window, dT);
+		bat1.Update(window, dT);
+		bat2.Update(window, dT);
 
-		// Simulation
-		Vec2 pos{ spr.getPosition() };
-		pos += vel * dT;
-
-		spr.setPosition(pos);
-
+		// Bounce the ball back
+		Bounce(bat1, bat2);
+		// Record player score
+		Record(window, dT);
 	}
-	void Render(sf::RenderWindow& window, float dT) override
+
+	void Render(sf::RenderWindow& window, float dT)
 	{
-		Obj2D::Render(window, dT);
+		// Renders
+		ball.Render(window, dT);
+		bat1.Render(window, dT);
+		bat2.Render(window, dT);
 	}
 };
 
@@ -294,18 +301,11 @@ int main()
 	sf::Texture texPong;
 	if (!texPong.loadFromFile("data/pongSprites.png"))
 		assert(false);
-	// Ball
-	Ball ball;
-	ball.Init(window, texPong);
 
-	// Players
-	Bat1 bat1;
-	bat1.Init(window, texPong);
-	Bat2 bat2;
-	bat2.Init(window, texPong);
-
-
+	Game game;
+	game.Init(window, texPong);
 	sf::Clock clock;
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -314,24 +314,16 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
 		float elapsedTime = clock.getElapsedTime().asSeconds();
 		clock.restart();
 
-		// Bounce 
-		//ball.ChangeXDirection(bat1.spr.getPosition().x);
-		//ball.ChangeXDirection(bat2.spr.getPosition().x);
-
 		// Updates
-		ball.Update(window, elapsedTime);
-		bat1.Update(window, elapsedTime);
-		bat2.Update(window, elapsedTime);
-		
+		game.Update(window, elapsedTime);
 		window.clear();
 		
 		// Renders
-		ball.Render(window, elapsedTime);
-		bat1.Render(window, elapsedTime);
-		bat2.Render(window, elapsedTime);
+		game.Render(window, elapsedTime);
 
 		window.display();
 	}
